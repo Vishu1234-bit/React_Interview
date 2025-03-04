e90156d55e05b272874a0d2c504f7f2f545c1919
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2025-03-04 04:38:46 -0800

    Create App.js
    The Holy Grail layout is a famous CSS page layout that has traditionally been hard to implement. It consists of a header, footer, and three columns. The left column contains navigation items, the middle column contains the page contents, and the right column contains ads.
    
    Holy Grail layout example
    
    Implement the Holy Grail layout using just CSS. You shouldn't need to change the HTML too much.
    
    Requirements
    Header
    Stretches horizontally across the whole page.
    60px tall.
    Columns
    Both the left and right columns have a fixed width of 100px.
    The center column is fluid-width.
    All the columns should have the same height, regardless of which column is the tallest.
    Footer
    Stretches horizontally across the whole page.
    100px tall.
    The footer should be at the bottom of the page even if there is not enough content to fill up the viewport height.

---
+++ Holy_grail/App.js
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <>
+      <header>Header</header>
+      <div className="holy-grail">
+        <nav>Navigation</nav>
+        <main>Main</main>
+        <aside>Sidebar</aside>
+      </div>
+      <footer>Footer</footer>
+    </>
+  );
+}
