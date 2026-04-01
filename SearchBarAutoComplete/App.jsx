6de6e1053d6d331225c760609658664a9e489288
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2026-04-02 02:17:27 +0530

    Create App.jsx

---
+++ SearchBarAutoComplete/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react'
+import SearchBar from "./SearchBar";
+function App() {
+
+  return (
+    <div>
+      <h2>Search AutoComplete</h2>
+      <SearchBar/>
+    </div>
+  )
+}
+
+export default App
