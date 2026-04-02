302ca8295d378d124c8c6b3196369cbebac9bea2
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2026-04-02 16:02:27 +0000

    Create App.js

---
+++ ProductListingPage/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import ProductPage from "./ProductPage";
+function App() {
+  return (
+    <div>
+      <h2>Product Listing Walmart</h2>
+      <ProductPage />
+    </div>
+  );
+}
+
+export default App;
