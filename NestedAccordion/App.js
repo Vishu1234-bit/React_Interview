d13811391abecafadc6b7fe51aa26ac665111b6b
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2026-04-03 18:58:57 +0530

    Create App.js

---
+++ NestedAccordion/App.js
@@ -0,0 +1,11 @@
+import AccordionContainer from "./AccordionContainer";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <AccordionContainer />
+    </div>
+  );
+}
