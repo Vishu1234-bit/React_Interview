e2c7aeb298c1d4e0e509263bd6b9789b1e60224b
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2026-04-04 17:07:50 +0530

    Update App.js

--- Todo List/App.js
+++ Todo List/App.js
@@ -1,40 +1,75 @@
-import {useState} from "react";
+import "./styles.css";
+import { useState } from "react";
 export default function App() {
-  const [tasks,setTasks] = useState([]);
-  const [inputValue,setInputValue] =useState("");
-  function handleTasks(){
-    if(inputValue.trim() === ""){
-      return 
+  const [todo, setTodo] = useState([]);
+  const [query, setQuery] = useState("");
+  const [filter, setFilter] = useState("All");
+  const handleTasks = (query) => {
+    if (query.trim() === "") {
+      return;
     }
-    setTasks([...tasks,inputValue])
-    setInputValue("");
-  }
-function deleteTasks(index){
-  setTasks(tasks.filter((_,i)=>i!==index));
-}
+    setTodo((prev) => [
+      ...prev,
+      { id: Date.now(), text: query, completed: false },
+    ]);
+    setQuery("");
+  };
+  const deleteTask = (index) => {
+    setTodo(todo?.filter((t) => t.id !== index));
+  };
+  const editTask = (id, newText) => {
+    setTodo((prev) =>
+      prev.map((todo) => (todo.id === id ? { ...todo, text: newText } : todo))
+    );
+  };
+  const completeTask = (id) => {
+    setTodo((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const filteredTodo = todo?.filter((task) => {
+    if (filter === "Active") return !task.completed;
+    if (filter === "Completed") return task.completed;
+    return true;
+  });
   return (
-    <div>
+    <div className="App">
       <h1>Todo List</h1>
+      <input
+        type="text"
+        placeholder="Enter Todo"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={() => handleTasks(query)}>Submit</button>
       <div>
-        <input 
-        type="text" 
-        value = {inputValue}
-        onChange={(e)=>setInputValue(e.target.value)}
-        placeholder="Add your task" />
-        <div>
-          <button onClick = {handleTasks}>Submit</button>
-        </div>
+        <button onClick={() => setFilter("All")}>All</button>
+        <button onClick={() => setFilter("Active")}>Active</button>
+        <button onClick={() => setFilter("Completed")}>Completed</button>
       </div>
-
-      {tasks.length>0 && 
-      <ul>
-       {tasks.map((task,index)=>(
-        <li key={index}>
-          <span>{task}</span>
-          <button onClick={()=>deleteTasks(index)}>Delete</button>
-        </li>
-        ))}
-      </ul>}
+      {filteredTodo?.length > 0 && (
+        <ul>
+          {filteredTodo.map((todoTasks) => (
+            <li key={todoTasks.id}>
+              <span>{todoTasks.text}</span>
+              <button onClick={() => deleteTask(todoTasks.id)}>Delete</button>
+              <button onClick={() => completeTask(todoTasks.id)}>
+                Mark Completed
+              </button>
+              <button
+                onClick={() => {
+                  const newtext = prompt("editTodo", todoTasks.text);
+                  if (newtext.trim() !== "") editTask(todoTasks.id, newtext);
+                }}
+              >
+                Edit
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
